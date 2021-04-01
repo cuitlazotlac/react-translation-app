bef0d8d497dbc5c4feacbdefb4fade4167a69cdf
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-03-31 22:35:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  function handleClick(lang) {
+    i18n.changeLanguage(lang);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{t('Title.text1')}</p>
+        <p>{t('Subtitle.text2')}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +25,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <div
+        style={{ width: "100%", padding: "2rem 0", backgroundColor: "black" }}
+      >
+        <button onClick={() => handleClick("en")}>English</button>
+        <button onClick={() => handleClick("ko")}>Korean</button>
+        <button onClick={() => handleClick("fr")}>French</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './i18next';
+
 ReactDOM.render(
+  <Suspense fallback={(<div>Loading....</div>)} >
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Suspense>,
   document.getElementById('root')
 );
 
